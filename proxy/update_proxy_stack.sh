0da8cf805a1e3072a6b1cacb81c25c2518090305
ThePhaseless <kukubaorch@gmail.com>
2024-01-06 22:35:46 +0100

    Remove unnecessary code for checking PROXY variable

--- proxy/update_proxy_stack.sh
+++ proxy/update_proxy_stack.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# if PROXY is not said, ask if script should continue
-
-if [ -z "$PROXY" ]; then
-    read -r -p "PROXY is not set, continue? (Y/n): " answer
-    case ${answer,,} in
-    n*)
-        echo "Cancelling..."
-        exit
-        ;;
-    esac
-fi
 
 # Check if proxy.env exists
 if [ ! -f ./.env ]; then
