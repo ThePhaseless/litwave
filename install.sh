0a7b3ac1832c118e2f92a79ec17ec604b1d97321
ThePhaseless <kukubaorch@gmail.com>
2024-01-06 21:47:15 +0100

    Refactor installation script to improve readability and remove unnecessary code

--- install.sh
+++ install.sh
@@ -3,20 +3,19 @@
 # ----------------------------------
 
 # Ask if this is a host or a proxy
+sudo chmod +x ./* -R
 echo "Is this a host or a proxy?"
 echo "1. Host"
 echo "2. Proxy"
 read -r -p "Enter your choice: " choice
 case $choice in
-[1]*)
-    chmod +x ./host/install.sh
-    ./host/install.sh
+    [1]*)
+        ./host/install.sh
     ;;
-[2]*)
-    chmod +x ./proxy/install.sh
-    ./proxy/install.sh
+    [2]*)
+        ./proxy/install.sh
     ;;
-*)
-    echo "Invalid choice"
+    *)
+        echo "Invalid choice"
     ;;
 esac
