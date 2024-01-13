663b4e1712b93be85630f36937a19051c1d36fc0
ThePhaseless <kukubaorch@gmail.com>
2024-01-13 16:28:44 +0100

    Fix permissions and update installation script

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 # ----------------------------------
 
 # Ask if this is a host or a proxy
-sudo chmod +x ./*.sh -R
+find . -type f -iname "*.sh" -exec chmod +x {} \;
 echo "Is this a host or a proxy?"
 echo "1. Host"
 echo "2. Proxy"
 read -r -p "Enter your choice: " choice
 case $choice in
-    [1]*)
-        ./host/install.sh
-    ;;
-    [2]*)
-        ./proxy/install.sh
-    ;;
-    *)
-        echo "Invalid choice"
-    ;;
+[1]*)
+	./host/install.sh
+	;;
+[2]*)
+	./proxy/install.sh
+	;;
+*)
+	echo "Invalid choice"
+	;;
 esac
