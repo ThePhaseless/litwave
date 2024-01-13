3942522823dbc7b2f9c300dcfbf022944f37b7eb
ThePhaseless <kukubaorch@gmail.com>
2024-01-13 16:27:14 +0100

    Remove upload_acme.sh from crontab and install VS Code

--- proxy/install.sh
+++ proxy/install.sh
@@ -7,19 +7,6 @@
 # Date:   January 1, 2024
 #
 
-# Check if upload_acme is in crontab, if not add it
-echo "Checking if upload_acme.sh is in crontab..."
-if crontab -l | grep -q 'upload_acme.sh'; then
-	echo "upload_acme.sh already in crontab..."
-else
-	echo "Adding upload_acme.sh to crontab..."
-	(
-		crontab -l 2>/dev/null
-		echo "0 4 * * * $PWD/upload_acme.sh"
-	) | crontab -
-	echo "Done..."
-fi
-
 # Install VS Code
 ./general/setup_vscode.sh
 
