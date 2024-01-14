84e05c49fa7d4c7a90bf25b9aae9c5f8dd0e2876
ThePhaseless <kukubaorch@gmail.com>
2024-01-14 20:47:09 +0100

    Update SAMBA configuration file paths

--- DMZ/setup_samba.sh
+++ DMZ/setup_samba.sh
@@ -4,6 +4,12 @@
 echo "Installing SAMBA..."
 sudo apt install samba wsdd -y
 
+# Update the SAMBA configuration file
+echo "Updating SAMBA configuration file..."
+# Replace STORAGE_PATH with $STORAGE_PATH and SSD_PATH with $SSD_PATH
+sed -i "s|STORAGE_PATH|$STORAGE_PATH|g" ./shares.conf
+sed -i "s|SSD_PATH|$SSD_PATH|g" ./shares.conf
+
 # Copy the SAMBA configuration file
 echo "Copying SAMBA configuration file..."
 sudo cp ./shares.conf /etc/samba/
