868bf44a3fc6a13007b51676097de2997feba2fb
ThePhaseless <kukubaorch@gmail.com>
2024-01-14 20:46:59 +0100

    Add check for CONFIG_PATH in run_portainer.sh script

--- DMZ/run_portainer.sh
+++ DMZ/run_portainer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if $CONFIG_PATH is set
+if [ -z "$CONFIG_PATH" ]; then
+	echo "CONFIG_PATH is not set. What is the path to your portainer config folder? (e.g. ~/config/Portainer)"
+	read -r CONFIG_PATH
+fi
+
 # Pull and run Portainer
 echo "Pulling and running Portainer in $CONFIG_PATH/Portainer..."
 docker run -d -p "9000:9000" --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v "$CONFIG_PATH"/Portainer:/data portainer/portainer-ce
