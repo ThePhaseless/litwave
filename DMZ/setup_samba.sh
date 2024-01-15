acd9f245ca90e927f44d19f02460d5d88dbd45e0
ThePhaseless <kukubaorch@gmail.com>
2024-01-15 13:38:36 +0000

    Update DMZ and General setup scripts

--- DMZ/setup_samba.sh
+++ DMZ/setup_samba.sh
@@ -6,13 +6,15 @@ sudo apt install samba wsdd -y
 
 # Update the SAMBA configuration file
 echo "Updating SAMBA configuration file..."
-# Replace STORAGE_PATH with $STORAGE_PATH and SSD_PATH with $SSD_PATH
-sed -i "s|STORAGE_PATH|$STORAGE_PATH|g" ./shares.conf
-sed -i "s|SSD_PATH|$SSD_PATH|g" ./shares.conf
 
 # Include the SAMBA configuration file
 echo "Including SAMBA configuration file..."
 "include = $PWD/shares.conf" | sudo tee -a /etc/samba/smb.conf
+
+# Replace CONFIG_PATH and STORAGE_PATH with $CONFIG_PATH and $STORAGE_PATH
+echo "Replacing CONFIG_PATH and STORAGE_PATH with \$CONFIG_PATH and \$STORAGE_PATH..."
+sudo sed -i "s|CONFIG_PATH|$CONFIG_PATH|g" shares.conf
+sudo sed -i "s|STORAGE_PATH|$STORAGE_PATH|g" shares.conf
 
 echo "Restarting SAMBA..."
 sudo systemctl restart smbd
