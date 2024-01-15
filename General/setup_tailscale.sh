7cef1cbb20ffc7a4c9c7ec4125514d7df6d46f65
ThePhaseless <kukubaorch@gmail.com>
2024-01-15 12:17:29 +0100

    Add Tailscale installation and setup script

--- General/setup_tailscale.sh
+++ General/setup_tailscale.sh
@@ -2,6 +2,15 @@
 
 # Install Tailscale
 echo "Installing Tailscale..."
-curl -fsSL https://tailscale.com/install.sh | sh
-echo "Done..."
+
+## Add tailscale repository
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+## Install tailscale
+sudo apt-get update
+sudo apt-get install tailscale
+
+## Run tailscale
 sudo tailscale up
+echo "Done..."
