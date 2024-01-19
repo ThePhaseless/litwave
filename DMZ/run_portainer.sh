8cf3aefe99b656e8f3720998a3be14f4ec0cf4df
ThePhaseless <kukubaorch@gmail.com>
2024-01-19 17:16:34 +0000

    Add traefik label to Portainer container

--- DMZ/run_portainer.sh
+++ DMZ/run_portainer.sh
@@ -8,5 +8,5 @@ fi
 
 # Pull and run Portainer
 echo "Pulling and running Portainer in $CONFIG_PATH/Portainer..."
-docker run -d -p "9000:9000" --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v "$CONFIG_PATH"/Portainer:/data portainer/portainer-ce
+docker run -d -p "9000:9000" --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v "$CONFIG_PATH"/Portainer:/data portainer/portainer-ce --label "traefik.routers.jellyseerr.rule=HostRegexp(\`{subdomain:portainer}.{host:.+}\`)"
 echo "Done..."
