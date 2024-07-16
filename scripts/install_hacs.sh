070bbb3a6ec86977d2b4a1612757ab57fec0e299
--help <kukubaorch@gmail.com>
2024-07-16 18:14:29 +0200

    write logic

---
+++ scripts/install_hacs.sh
@@ -0,0 +1,7 @@
+source .env
+config_path=$CONFIG_PATH
+
+if [ -d "$config_path"/Config/Home/HomeAssistant/custom_components/hacs ]; then
+    docker exec -it HomeAssistant "wget -O - https://get.hacs.xyz | bash -"
+    docker restart HomeAssistant
+fi
