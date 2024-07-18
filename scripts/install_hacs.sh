1b36c0113bb643485c11400093aa38a46db1e4de
Thephaseless <kukubaorch@gmail.com>
2024-07-18 22:58:36 +0000

    linting fixes

--- scripts/install_hacs.sh
+++ scripts/install_hacs.sh
@@ -6,6 +6,6 @@ config_path=$CONFIG_PATH
 
 if [ ! -d "$config_path"/Config/Home/HomeAssistant/custom_components/hacs ]; then
     command="wget -O - https://get.hacs.xyz | bash"
-    echo $command | docker exec -i HomeAssistant bash
+    echo "$command" | docker exec -i HomeAssistant bash
     docker restart HomeAssistant
 fi
