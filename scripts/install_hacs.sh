f99bb75cdeeaf74c16b046a9e537a0c684b8c326
Thephaseless <kukubaorch@gmail.com>
2024-10-08 12:34:04 +0200

    fix hacs installer

--- scripts/install_hacs.sh
+++ scripts/install_hacs.sh
@@ -2,9 +2,8 @@
 set -e
 
 source .env
-config_path=$CONFIG_PATH
 
-if [ ! -d "$config_path"/Config/Home/HomeAssistant/custom_components/hacs ]; then
+if [ ! -d "$CONFIG_PATH"/Home/HomeAssistant/custom_components/hacs ]; then
     command="wget -O - https://get.hacs.xyz | bash"
     echo "$command" | docker exec -i HomeAssistant bash
     docker restart HomeAssistant
